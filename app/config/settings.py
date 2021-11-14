4863e25772dac2eb265605e3c46c3023dc4aa0b0
MMartynamajchrzak <mmartynamajchhrzak@wp.pl>
2021-11-15 00:33:37 +0100

    test commit

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,9 +6,6 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 env = environ.Env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY", default='')
